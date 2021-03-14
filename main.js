ab9632f551de819e3de7bd85e70dfdc0d356def1
Cristin O'Connor <her@cristin.io>
2021-03-14 13:49:00 -0600

    My code for completed L2: https://www.vuemastery.com/courses/intro-to-vue-3/creating-the-vue-app-vue3

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Vue is REACTIVE: When a data value changes, anywhere relying on that data will automatically update for us. We don’t have to do anything to make that happen.'
+    }
+  },
+});
